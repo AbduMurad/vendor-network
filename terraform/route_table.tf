6628d9fb2fe2fd0199506153441e3a6a32da0ee2
AbduMurad <abadeem@gmail.com>
2023-06-20 19:22:45 +0200

    added rt

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -15,7 +15,7 @@ resource "aws_route" "public_1" {
 
 resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route.public_1.id
+  route_table_id = aws_route_table.public_1.id
 }
 
 # public 2
@@ -36,7 +36,7 @@ resource "aws_route" "public_2" {
 
 resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route.public_2.id
+  route_table_id = aws_route_table.public_2.id
 }
 
 # private 1
@@ -57,7 +57,7 @@ resource "aws_route" "private_1" {
 
 resource "aws_route_table_association" "private_1" {
   subnet_id      = aws_subnet.private_1.id
-  route_table_id = aws_route.private_1.id
+  route_table_id = aws_route_table.private_1.id
 }
 
 # private 2
@@ -78,5 +78,5 @@ resource "aws_route" "private_2" {
 
 resource "aws_route_table_association" "private_2" {
   subnet_id      = aws_subnet.private_2.id
-  route_table_id = aws_route.private_2.id
+  route_table_id = aws_route_table.private_2.id
 }
